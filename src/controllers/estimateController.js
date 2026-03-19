89701ab7f653a725aa52c31e11e8a9790d7f7808
LHQH <loic.HQH@outlook.fr>
2026-03-19 16:12:06 +0100

    CRUD estimat/bill MODELE MODALE CLIENT

--- src/controllers/estimateController.js
+++ src/controllers/estimateController.js
@@ -3,9 +3,35 @@ import { adapter } from "../../prisma/adapter.js";
 
 const prisma = new PrismaClient({ adapter });
 
-
+//VIEW DEVIS
 export async function getEstimate(req, res) {
     res.render("pages/estimate.twig", {
         title: "Gestion des devis"
     })
+}
+
+// NEW DEVIS
+export async function newEstimate(req, res) {
+    try {
+        const { reference,id_client,tva,description,QTY,unitAmount } = req.body
+        await prisma.estimate.create({
+            data: {
+                estimatTitle,
+                reference,
+                id_client,
+                tva,
+                description,
+                QTY,
+                unitAmount,
+                // LIAISON DE facture A SON ARTISAN
+                createdBy: { connect: { id: req.craftman.id } }
+            }
+        })
+        res.redirect("/estimatell/list")
+    } catch (error) {
+        console.log(error);
+        res.render("pages/estimate.twig", {
+            error: "Erreur lors de la création d'une facture"
+        })
+    }
 }
--- src/routes/estimateRouter.js
+++ src/routes/estimateRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { authguard } from "../services/authguard.js"
-import { getEstimate } from "../controllers/estimateController.js";
+import { getEstimate, newEstimate } from "../controllers/estimateController.js";
 
 
 export const estimateRouter = express.Router()
 
 estimateRouter.get("/list", authguard, getEstimate)
+estimateRouter.post("/new", authguard, newEstimate)
